89a08eed60088e31388642f3bd310c38555f9899
EFZY <3307133389@qq.com>
2025-03-15 21:42:14 +0800

    LCD在ESP32

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,39 @@
+
+ 
 #include <Arduino.h>
+#include <TFT_eSPI.h> // Graphics and font library for ST7735 driver chip
+#include <SPI.h>
+#include <Servo.h>
 
-// put function declarations here:
-int myFunction(int, int);
+TFT_eSPI tft = TFT_eSPI();  // Invoke library, pins defined in User_Setup.h
 
-void setup() {
-  // put your setup code here, to run once:
-  //Serial.begin(9600);
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH);
-  int result = myFunction(2, 3);
+#define TFT_GREY 0xBDF7
+
+void setup(void) {
+      // 初始化串口用于调试
+      Serial.begin(115200);
+      Serial.println("Initializing...");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    delay(1000);
+    static bool initialized = false;
+    if (!initialized) {
+      // 初始化 TFT 屏幕
+      tft.init();
+      tft.setRotation(0);
+      tft.fillScreen(TFT_GREY);
+      tft.setTextColor(TFT_GREEN, TFT_GREY);  // 设置文本颜色和背景颜色
+    
+      // 在屏幕中央显示文本
+      //tft.drawCentreString("Hello, World!", 64, 64, 4);
+      tft.println("Hello, Worldddd!\n");
+      
+      initialized = true;
+      Serial.println("Initialization done.");
+    }
+  
+
+  // Nothing to do here
 }
